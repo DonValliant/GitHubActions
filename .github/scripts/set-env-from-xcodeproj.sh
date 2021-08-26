764bc9cad4881af785f88ff6bc7c32fdab835a8b
Donald Valliant <donaldvalliant@Donalds-Mac-mini-2.local>
2021-08-26 09:59:57 -0400

    fixing export statement and adding an echo

--- .github/scripts/set-env-from-xcodeproj.sh
+++ .github/scripts/set-env-from-xcodeproj.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 set -euo pipefail
-set ACTIONS_ALLOW_UNSECURE_COMMANDS=true
+export ACTIONS_ALLOW_UNSECURE_COMMANDS=true
+echo $ACTIONS_ALLOW_UNSECURE_COMMANDS
 SCHEME="$(xcodebuild -list -json | jq -r '.project.schemes[0]')"
 PRODUCT_NAME="$(xcodebuild -scheme "$SCHEME" -showBuildSettings | grep " PRODUCT_NAME " | sed "s/[ ]*PRODUCT_NAME = //")"
 echo "::set-env name=PRODUCT_NAME::$PRODUCT_NAME"
