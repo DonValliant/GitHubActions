1f11d7547f2bcc0085d0facb83a41a35d45467b3
Donald Valliant <donaldvalliant@Donalds-Mac-mini-2.local>
2021-08-26 10:19:07 -0400

    cleaning up shell

--- .github/scripts/set-env-from-xcodeproj.sh
+++ .github/scripts/set-env-from-xcodeproj.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 set -euo pipefail
-export ACTIONS_ALLOW_UNSECURE_COMMANDS=true
-echo $ACTIONS_ALLOW_UNSECURE_COMMANDS
 SCHEME="$(xcodebuild -list -json | jq -r '.project.schemes[0]')"
 PRODUCT_NAME="$(xcodebuild -scheme "$SCHEME" -showBuildSettings | grep " PRODUCT_NAME " | sed "s/[ ]*PRODUCT_NAME = //")"
 echo "::set-env name=PRODUCT_NAME::$PRODUCT_NAME"
