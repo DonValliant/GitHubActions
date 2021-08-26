87699472955ed69b013944b742b7cbe73dca8366
Donald Valliant <donaldvalliant@Donalds-Mac-mini-2.local>
2021-08-26 09:51:55 -0400

    allowing unsecure stuff.

--- .github/scripts/set-env-from-xcodeproj.sh
+++ .github/scripts/set-env-from-xcodeproj.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -euo pipefail
-
+set env ACTIONS_ALLOW_UNSECURE_COMMANDS: true
 SCHEME="$(xcodebuild -list -json | jq -r '.project.schemes[0]')"
 PRODUCT_NAME="$(xcodebuild -scheme "$SCHEME" -showBuildSettings | grep " PRODUCT_NAME " | sed "s/[ ]*PRODUCT_NAME = //")"
 echo "::set-env name=PRODUCT_NAME::$PRODUCT_NAME"
