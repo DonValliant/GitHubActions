8c26d5dcf92750c9abbd8d36b74bd4c357b7257a
Donald Valliant <donaldvalliant@Donalds-Mac-mini-2.local>
2021-08-30 16:23:32 -0400

    adding scan shell

---
+++ .github/scripts/parseForScan.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# fail if any commands fails
+set -e
+# debug log
+set -x
+# write your script here
+# clone and generate xml file using xccov-to-sonarqube-generic.sh
+git clone https://github.com/SonarSource/sonar-scanning-examples.git
+bash ~/git/sonar-scanning-examples/swift-coverage/swift-coverage-example/xccov-to-sonarqube-generic.sh /Users/runner/work/GitHubActions/GitHubActions/.build/x86_64-apple-macosx/debug/GitHubActionsPackageTests.xctest > temp-coverage.xml
+# strip Objective C out of file
+cat temp-coverage.xml | sh parseSonarShellResult.sh> sonarqube-generic-coverage.xml
+
+echo "WHERE ARE WE"
+pwd
+ls -laF
