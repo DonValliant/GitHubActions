a13b5d9c2cf55dc7e1de9eb3e81c63decb0eb233
Donald Valliant <donaldvalliant@Donalds-Mac-mini-2.local>
2021-08-31 09:49:14 -0400

    starting a build to find xctest

--- .github/scripts/parseForScan.sh
+++ .github/scripts/parseForScan.sh
@@ -6,6 +6,7 @@ set -x
 # write your script here
 # clone and generate xml file using xccov-to-sonarqube-generic.sh
 pwd
+cd .build
 ls -laF
 git clone https://github.com/SonarSource/sonar-scanning-examples.git
 bash /Users/runner/work/GitHubActions/GitHubActions/sonar-scanning-examples/swift-coverage/swift-coverage-example/xccov-to-sonarqube-generic.sh /Test.xcresult > sonarqube-generic-coverage.xml
